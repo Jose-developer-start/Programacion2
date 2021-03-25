1480ddd84144811e4492a6183469a777f6b24aca
Jose Deo <josedeodanes99@gmail.com>
2021-03-25 11:31:12 -0600

    Update}

--- views/admin/procesos_varios/categorias/principal_categorias.php
+++ views/admin/procesos_varios/categorias/principal_categorias.php
@@ -1,7 +1,15 @@
-<table>
-    <thead>
-        <tr>
-            <th>#</th>
-        </tr>
-    </thead>
-</table>
+<head>
+    <script src="../../public/js/funciones-navbar.js"></script>
+    <script src="../../public/js/funciones-categorias.js"></script>
+</head>
+
+
+<?php
+    session_start();
+    include_once "../../../../models/conexion.php";
+    include_once "../../../../models/procesos.php";
+    include_once "../../../../controllers/procesos.php";
+
+    $cont = 0;
+    $pagina = 0;
+?>
