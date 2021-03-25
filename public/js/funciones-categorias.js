9c6421f342623f41ff7c3c599b139ca2bc362d6c
Jose Deo <josedeodanes99@gmail.com>
2021-03-25 11:09:14 -0600

    Update

--- public/js/funciones-categorias.js
+++ public/js/funciones-categorias.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Funcion para cargar la vista de principal categorias
     $("a.categorias").click(function(event){
         $("#contenido-procesos").load("procesos_varios/categorias/principal_categorias.php");
         event.preventDefault();
